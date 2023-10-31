79b9ca0f69d974d402328545c2e24d002d3a7ed7
Jean Huynh <huynhjeanpierre@hotmail.com>
2023-10-31 20:23:19 +0100

    include breaktime

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ load_dotenv()
 wb = Workbook()
 ws = wb.active
 
-ws.append(["Date", "Location", "Start", "End", "Hours"])
+ws.append(["Date", "Location", "Start", "End", "Break", "Hours"])
 
 ICS = requests.get(os.getenv('SCHEDULE_ICS_URL')).text.split('\r\n')
 
@@ -21,7 +21,7 @@ i = 0
 while i < lines_nb:
     if ICS[i].startswith('DTSTART;'):
         start_date, start_time = parse_timedate(ICS[i])
-        if start_date.month == 7:
+        if start_date.month == 10:
             while not ICS[i].startswith('DTEND;'):
                 i += 1
             end_date, end_time = parse_timedate(ICS[i])
@@ -33,11 +33,11 @@ workdays.sort(key=lambda workday: workday[0])
 row = 2
 for workday in workdays:
     start_date, location, start_time, end_time = workday
-    ws.append([start_date.strftime('%d/%m/%Y'), location, start_time, end_time, f'=D{row} - C{row}'])
+    ws.append([start_date.strftime('%d/%m/%Y'), location, start_time, end_time, f'=IF((D{row} - C{row}) < TIME(5, 0, 0), "00:00:00", "00:30:00"', f'=D{row} - C{row} - E{row}'])
     row += 1
 
 ws[f'A{row}'] = "Total"
-ws[f'E{row}'] = f'=SUM(E2:E{row - 1})'
+ws[f'F{row}'] = f'=SUM(F2:F{row - 1})'
 
 style_worksheet(ws, row, 5)
 
